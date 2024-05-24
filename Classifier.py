a74f0015ab95661101a881fd81bbb79b53996671
ajbuwidnnfjwj <416tommy@naver.com>
2024-05-24 17:20:42 +0900

    vgg16 input

--- Classifier.py
+++ Classifier.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torchvision import models, transforms
-from torch.utils.data import Dataset, DataLoader
-from PIL import Image
+from torch.utils.data import DataLoader
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # 이미지 전처리 및 데이터셋 생성
 transform = transforms.Compose([
@@ -14,49 +14,52 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
 trainset = torchvision.datasets.ImageFolder(root='./FaceID/images', transform=transform)
-print(trainset.__getitem__(0), len(trainset))
-# dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+dataloader = DataLoader(trainset, batch_size=32, shuffle=True)
 
-# # 사전 훈련된 VGG 모델 불러오기
-# vgg_model = models.vgg16(pretrained=True)
-# for param in vgg_model.parameters():
-#     param.requires_grad = False
+# 사전 훈련된 VGG 모델 불러오기
+vgg_model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+for param in vgg_model.parameters():
+    param.requires_grad = False
 
-# # 새로운 분류 레이어 추가
-# num_features = vgg_model.classifier[6].in_features
-# vgg_model.classifier[6] = nn.Sequential(
-#     nn.Linear(num_features, 512),
-#     nn.ReLU(),
-#     nn.Dropout(0.5),
-#     nn.Linear(512, 2),
-#     nn.Softmax(dim=1)
-# )
+# 새로운 분류 레이어 추가
+num_features = vgg_model.classifier[6].in_features
+vgg_model.classifier[6] = nn.Sequential(
+    nn.Linear(num_features, 512),
+    nn.ReLU(),
+    nn.Dropout(0.5),
+    nn.Linear(512, 2),
+    nn.Softmax(dim=1)
+)
 
-# # 모델을 GPU 또는 CPU로 이동
-# vgg_model = vgg_model.to(device)
+# 모델을 GPU 또는 CPU로 이동
+vgg_model = vgg_model.to(device)
 
-# # 손실 함수 및 옵티마이저 정의
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.Adam(vgg_model.parameters(), lr=0.001)
+# 손실 함수 및 옵티마이저 정의
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(vgg_model.parameters(), lr=0.001)
 
-# # 모델 학습
-# num_epochs = 10
-# for epoch in range(num_epochs):
-#     running_loss = 0.0
-#     for images in dataloader:
-#         images = images.to(device)
-#         labels = torch.tensor([1] * images.size(0) if "positive" in image_paths else [0] * images.size(0)).to(device)
+# 모델 학습
+num_epochs = 10
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, data in enumerate(trainset, 0):
+        # 입력 데이터와 레이블을 GPU로 이동
+        inputs, labels = data[0].to(device), torch.FloatTensor(data[1]).to(device)
+        print(inputs.shape)
+        # 파라미터 그라디언트 초기화
+        optimizer.zero_grad()
 
-#         optimizer.zero_grad()
+        # 순전파 + 역전파 + 최적화
+        outputs = vgg_model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-#         outputs = vgg_model(images)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-
-#         running_loss += loss.item()
-
-#     print(f"Epoch {epoch+1}/{num_epochs}, Loss: {running_loss}")
+        # 통계 출력
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # 매 2000 미니배치마다
+            print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
 
 # # 학습된 모델 저장
 # torch.save(vgg_model.state_dict(), "face_classification_model.pth")
--- FaceID.py
+++ FaceID.py
@@ -1,27 +1,13 @@
 import cv2 as cv
 import numpy as np
 import ImageProcess
+import os
 
-isa_1 = ImageProcess.getFacePart(cv.imread("./FaceID/images/isa_1.jpg"))
-isa = ImageProcess.getFacePart(cv.imread("./FaceID/images/isa_2.jpg"))
-print(np.array(isa))
+path = '.\FaceID\images\\isa'
+files = os.listdir(path)
 
-keypoints_s, descriptors_s = ImageProcess.keyPoints(isa_1)
-keypoints_i, descriptors_i = ImageProcess.keyPoints(isa)
-
-matcher = cv.BFMatcher()
-matches = matcher.knnMatch(descriptors_s, descriptors_i, k = 2)
-
-good_match = sorted([m for m, _ in matches], key = lambda x : x.distance)[:100]
-print(np.array(good_match))
-
-points1 = [keypoints_s[m.queryIdx].pt for m in good_match]
-points2 = [keypoints_i[m.trainIdx].pt for m in good_match]
-
-image_matches = cv.drawMatches(isa_1, keypoints_s, isa, keypoints_i, good_match, None)
-
-# 결과 이미지를 화면에 표시합니다.
-cv.imshow('Correspondences', image_matches)
-cv.waitKey(0)
-cv.destroyAllWindows()
-
+for file_name in files:
+    image_name = os.path.join(path, file_name)
+    image = cv.imread(image_name)
+    # image = ImageProcess.getFacePart(image)
+    cv.imwrite(image_name, image)
